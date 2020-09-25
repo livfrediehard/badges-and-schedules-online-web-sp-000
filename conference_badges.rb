ec2ef16cb6160f014b45d45bf994c26ba9f36a54
Brian Palmer <bpalmer710@gmail.com>
2020-09-25 01:52:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,10 @@ def batch_badge_creator(attendees)
 end
 end
 
-ddef assign_rooms(attendees)
+def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
-end
+  end
 end
 
 def printer(attendees)
@@ -22,5 +22,5 @@ def printer(attendees)
     end
     assign_rooms(attendees).each do |assignment|
         puts assignment
-end
+    end
 end
