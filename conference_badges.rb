4480a7ea6e9bc4ca06a835c404186b99bdd6a315
Brian Palmer <bpalmer710@gmail.com>
2020-09-25 01:52:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,17 @@ def batch_badge_creator(attendees)
 end
 end
 
-def assign_rooms(attendees)
-  counter = 1 
-  attendees.each do |attendee|
-    "Hello, #{attendee}! You'll be assigned to room #{counter}!"
-    counter += 1 
+ddef assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
 end
 end
