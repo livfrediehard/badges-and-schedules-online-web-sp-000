6b3c666c429abc77fec492fb237b6b067c90ff89
Brian Palmer <bpalmer710@gmail.com>
2020-09-25 01:43:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,10 +8,12 @@ def batch_badge_creator(attendees)
   attendees.each do |attendee|
    puts "Hello my name is #{attendee}."
 end
+end
 
 def assign_rooms(attendees)
   counter = 1 
   attendees.each do |atendee|
     puts "Hello, #{atendeee}! You'll be assigned to room #{counter}!"
     counter += 1 
+end
 end
