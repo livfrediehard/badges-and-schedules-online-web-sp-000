5c721594f22eb794824b712fce731804cbae7154
Brian Palmer <bpalmer710@gmail.com>
2020-09-25 01:28:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
+def badge_maker(name)
+  puts "Hello, may name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  
+end
+
+end
+
